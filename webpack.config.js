9c6dde3575e2858287e4614c6156b73708b1b2ab
nayesoum <104022857+nayesoum@users.noreply.github.com>
2023-11-12 17:50:49 +0100

    correction publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    publicPath: 'mon-cv-gh-pages',
+    publicPath: '/mon-cv-gh-pages/',
     filename: 'bundle.js',
   },
   module: {
